2406b4cf371ca0ec2b6b2d732de8ddeed444f58a
paygle <dodar@qq.com>
2018-03-03 18:13:56 +0800

    更新form-table

--- packages/form-table-item/index.js
+++
@@ -1,8 +0,0 @@
-import ElFormTableItem from '../form-table/src/table-form-item';
-
-/* istanbul ignore next */
-ElFormTableItem.install = function(Vue) {
-  Vue.component(ElFormTableItem.name, ElFormTableItem);
-};
-
-export default ElFormTableItem;
